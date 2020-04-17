e9a49c4e04429d719bd46cf2a8afe8fb06edd694
Rudy0 <paduraru.rudolf.vasile@gmail.com>
2020-04-17 12:00:10 +0300

    added post on server

--- src/server/index.js
+++ src/server/index.js
@@ -11,25 +11,34 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('dist'));
 
 console.log(__dirname);
 
-var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-    });
-
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('App listening on port 3000!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
+
+app.post("/data", function(req, res) {
+	var textapi = new aylien({
+        application_id: process.env.API_ID,
+        application_key: process.env.API_KEY
+        });
+        textapi.sentiment({
+            'text': req.body.text
+          }, function(error, response) {
+            if (error === null) {
+                res.send(response);
+            }
+          }); 
+});
